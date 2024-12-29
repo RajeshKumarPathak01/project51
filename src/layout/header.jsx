8ed510b36eb0084ab0fb11d51f7e474c02dc00f1
rajeshkumarpathak01 <rajprofessional42@gmail.com>
2024-12-29 19:24:04 +0530

    allignment

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -8,19 +8,19 @@ function Header() {
   const toggleMenu = (isOpen) => setIsOpen(!isOpen);
 
   return (
-    <div className="container flex flex-wrap items-center justify-between mx-0 sm:mx-3  bg-white rounded-md sticky sm:top-0  top-0 border-b-[2px]">
+    <div className="container flex flex-wrap items-center justify-between mx-0 sm:mx-2  bg-white rounded-md sticky sm:top-0  top-0 border-b-[2px]">
       <div className=" -mr-6 text-xl flex    ">
         <img className=" h-[90px] w-[90px] " src={logoIcon} alt="Logo" />{" "}
         <span className=" py-8 text-2xl font-bold "> Movie world</span>
       </div>
  
       <div
-        className={` sm:flex sm:items-center sm:mx-0  py-1 transition-all duration-300 ease-in-outflex flex-wrap bg-blue-900 text-white rounded-lg sm:-mr-[10%] -mr-[38%] 
+        className={` sm:flex sm:items-center sm:mx-0  py-1   flex flex-wrap bg-blue-900 text-white rounded-lg sm:-mr-[30%] -mr-[38%] 
          
         }`}
       >
         <nav>
-          <ul className="flex flex-row   gap-3 flex-wrap mx-0 sm:-mx-1 sm:gap-0  text-lg font-medium py-2 ">
+          <ul className="flex flex-row   flex-wrap mx-0 sm:-mx-1 sm:gap-0  text-lg font-medium py-2 ">
             <li>
               <NavLink to="/">Home</NavLink>
             </li>
